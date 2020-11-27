4a06a4e150c5cd565877cf2c38e14ed0a2ba4592
Muhammad Usman <muhammadusman1794@gmail.com>
2020-11-27 17:14:29 +0500

    Adds tooltip for Coverage and Confirmed widgets

--- src/Components/WorldMap/WorldMapInner.jsx
+++ src/Components/WorldMap/WorldMapInner.jsx
@@ -13,12 +13,24 @@ const WorldMapInner = ({
   activeCountry,
 }) => {
   const chinaTooltip = React.useRef(null);
+  const test = React.useRef(null);
   const other = React.useRef(null);
   const { features = [] } = countries;
+
   React.useEffect(() => {
     ReactTooltip.rebuild();
     ReactTooltip.show(chinaTooltip.current);
   });
+  React.useEffect(() => {
+    if (activeCountry) {
+      ReactTooltip.show(test.current);
+      ReactTooltip.rebuild();
+    } else {
+      ReactTooltip.hide(test.current);
+      ReactTooltip.rebuild();
+    }
+  }, [activeCountry]);
+
   const color = getColor(5, 10000);
   return (
     <>
@@ -37,7 +49,13 @@ const WorldMapInner = ({
             return (
               <path
                 key={`country${i}`}
-                ref={obj.properties.name === "China" ? chinaTooltip : other}
+                ref={
+                  obj.properties.name === "China"
+                    ? chinaTooltip
+                    : obj.properties.name === activeCountry
+                    ? test
+                    : other
+                }
                 d={path(obj)}
                 fill={color(confirmed)}
                 stroke={
@@ -52,6 +70,8 @@ const WorldMapInner = ({
                 data-for={
                   obj.properties.name === "China"
                     ? "first-click"
+                    : obj.properties.name === activeCountry
+                    ? "test"
                     : "second-click"
                 }
                 data-html
@@ -76,6 +96,35 @@ const WorldMapInner = ({
         multiline
         className="tooltip"
       />
+      {activeCountry && (
+        <ReactTooltip
+          id="test"
+          place="top"
+          afterHide={() => (test.current = null)}
+          overridePosition={(e, _, b) => {
+            const isUS = activeCountry === "United States";
+            const isRNZ =
+              activeCountry === "Russian Federation" ||
+              activeCountry === "New Zealand";
+
+            const {
+              height: pHeight = 50,
+              width: pWidth,
+              left,
+              right,
+              top,
+            } = b.getBoundingClientRect();
+            const off = pWidth / 7;
+            console.log(b.getBoundingClientRect(), e);
+            return {
+              left: isUS ? left + off : isRNZ ? right - off - off : e.left,
+              top: isRNZ ? top + pHeight / 3 : e.top + pHeight / 2,
+            };
+          }}
+          multiline
+          className="tooltip"
+        />
+      )}
       <ReactTooltip id="second-click" multiline data-html className="tooltip" />
     </>
   );
